7d67105fb964dafc1f0c319bf52fa9aa9b0a7680
vanshika704 <sharmavanshi704@gmail.com>
2023-11-09 22:51:02 +0530

    normal commit

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:navigationapp/Welcome.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+          width: size.width,
+          height: size.height,
+          color: Color.fromARGB(255, 247, 69, 69),
+          child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                Image.asset('assets/diary.jpg'),
+                const SizedBox(height: 30),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Welcome()));
+                    },
+                    child: Container(
+                        height: 50,
+                        width: size.width * 0.7,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: const Center(
+                          child: Text('Daily diary entery'),
+                        )))
+              ]))),
+    );
+  }
+}
